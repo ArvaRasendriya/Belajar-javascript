c2baf71459d766758eb72ced3edfaf7da10357c6
ArvaRasendriya <arva.rasendriya@gmail.com>
2024-01-29 13:45:45 +0700

    object

--- Array.js
+++ Array.js
@@ -1,5 +1,6 @@
 // var nama array = new array() 
 var lemari = new Array("dompet", 983000, true);
+console.log(lemari[0]);
 console.log(lemari);
 
 lemari[3] = "ktp";
---
+++ object.js
@@ -0,0 +1,6 @@
+var orang = new Object();
+orang.nama = "Alexsandriya";
+orang.alamat = "Bandung";
+orang.tglLahir = "08122007";
+alert(orang.nama);
+console.log(orang);
