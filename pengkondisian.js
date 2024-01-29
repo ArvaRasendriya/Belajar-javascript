a40913ac45da9319ec51dc0856859a1003a2bb4c
ArvaRasendriya <arva.rasendriya@gmail.com>
2024-01-29 14:04:58 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var nama = prompt("masukan angka : ");
+// alert("selamat datang " + nama);
+
+if(x % 2 == 0) {
+    alert(x + "bilangan genap");
+}   else if (x % 2 == 1) {
+    alert(x + "bilangan ganjil");
+} else {
+    alert("data bukan anggka");
+}
